7b5c856e2fe4f7ed742d0ca141a590343fee553c
Elbert Ballo <eballofullstackdev@gmail.com>
2020-06-08 09:53:22 -0700

    Updates

---
+++ precache-manifest.a530d600788ffd2598ad6d26650e724c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6933214f455326cd11a96a7c35ee5a0e",
+    "url": "/elbert-spaceship-graphql/index.html"
+  },
+  {
+    "revision": "a419dc546749f5176058",
+    "url": "/elbert-spaceship-graphql/static/js/2.d500ea3c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/elbert-spaceship-graphql/static/js/2.d500ea3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d4b42deb557df17f8a",
+    "url": "/elbert-spaceship-graphql/static/js/main.49246813.chunk.js"
+  },
+  {
+    "revision": "e5635d65c39140ab1a23",
+    "url": "/elbert-spaceship-graphql/static/js/runtime-main.33e307c1.js"
+  },
+  {
+    "revision": "07d5ba4a894c77d6f799ecde7e84850f",
+    "url": "/elbert-spaceship-graphql/static/media/cart.07d5ba4a.svg"
+  },
+  {
+    "revision": "a8a3ba3a10003218c2a017cf68d20489",
+    "url": "/elbert-spaceship-graphql/static/media/curve.a8a3ba3a.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/elbert-spaceship-graphql/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/elbert-spaceship-graphql/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/elbert-spaceship-graphql/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "98f2a8b1296f385203e7b9b5d9938fcf",
+    "url": "/elbert-spaceship-graphql/static/media/exit.98f2a8b1.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/elbert-spaceship-graphql/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "3192f86cb3a13b08200f5e3d3fef0325",
+    "url": "/elbert-spaceship-graphql/static/media/home.3192f86c.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/elbert-spaceship-graphql/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "0c7bf061e88c942e5031e971c6ab447b",
+    "url": "/elbert-spaceship-graphql/static/media/logo.0c7bf061.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/elbert-spaceship-graphql/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "ea589b574fff44db577387da01243704",
+    "url": "/elbert-spaceship-graphql/static/media/profile.ea589b57.svg"
+  },
+  {
+    "revision": "c27cc1f53fed31cbbc1d743f9bbaba1d",
+    "url": "/elbert-spaceship-graphql/static/media/rocket.c27cc1f5.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/elbert-spaceship-graphql/static/media/space.7dfc7923.jpg"
+  }
+]);
